f9030eae9abd70cbf8532d9e230e7c982e567eab
Lucas Ricardo <lucasricardobarb@gmail.com>
2023-06-10 20:13:08 -0300

    adding contact and footer sections

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+var smallImage = './assets/home-cars-small.png';
+var normalImage = './assets/home-image.png';
+
+var image;
+
+var larguraJanela = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+if (larguraJanela <= 768) {
+  image = smallImage;
+} else if (larguraJanela > 768) {
+    image = normalImage;
+} else {
+    image = normalImage;
+}
+
+var imageElement = document.querySelector('#home-image');
+imageElement.src = image;
+
 let menu = document.querySelector("#menu-btn");
 let navbar = document.querySelector(".navbar");
 
@@ -79,22 +99,30 @@ var swiper = new Swiper(".featured-slider", {
     },
 });
 
-var smallImage = './assets/home-cars-small.png';
-var normalImage = './assets/home-image.png';
-
-var image;
-
-var larguraJanela = window.innerWidth
-|| document.documentElement.clientWidth
-|| document.body.clientWidth;
-
-if (larguraJanela <= 768) {
-  image = smallImage;
-} else if (larguraJanela > 768) {
-    image = normalImage;
-} else {
-    image = normalImage;
-}
-
-var imageElement = document.querySelector('#home-image');
-imageElement.src = image;
+var swiper = new Swiper(".reviews-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    centeredSlides: true,
+    grabCursor: true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
